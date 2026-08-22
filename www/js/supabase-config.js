e61e44868e11af6c0c610e48b29d9d1eb101a14e
gandhiiii <kushgandhi1995@gmail.com>
2026-08-22 23:52:00 +0530

    fix: restore valid Supabase publishableKey and add local account fallback to eliminate Cloud error on login page

--- www/js/supabase-config.js
+++ www/js/supabase-config.js
@@ -14,7 +14,7 @@
 
 var SUPABASE_CONFIG = {
     url:           "https://hpjexrelsmdjkjgohele.supabase.co",
-    publishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVC"
+    publishableKey: "sb_publishable_LJTo5vD8aKXiW5rhBaUAtA_sZYWNa2K"
 };
 
 (function () {
